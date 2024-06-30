27018ae69170d4ba36a47d3d72fdc518c2bd89ee
Asib Hasan Riyad <asibhasanriyad@gmail.com>
2024-06-30 08:28:15 +0600

    Your commit message

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -31,6 +31,8 @@ export const store = configureStore({
     }).concat(baseApi.middleware),
 });
 
+//
+
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 

89abbbf0886f8f39401cc4726b8e6918aca0085b
Asib Hasan Riyad <asibhasanriyad@gmail.com>
2024-07-04 00:59:49 +0600

    non serializable object error handeled

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,54 @@
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/features/auth/authApi";
+import { useAppDispatch } from "../redux/hooks";
+import { setUser } from "../redux/features/auth/authSlice";
+import { verifyToken } from "../utils/verifyToken";
+
+type TData = {
+  id: string;
+  password: string;
+};
 const Login = () => {
-  return <div>Login</div>;
+  const dispatch = useAppDispatch();
+  const { handleSubmit, register } = useForm({
+    defaultValues: {
+      id: "A-0001",
+      password: "12345",
+    },
+  });
+
+  const [login] = useLoginMutation();
+
+  const onsubmit = async (data: TData) => {
+    const userInfo = {
+      id: data.id,
+      password: data.password,
+    };
+    const res = await login(userInfo).unwrap();
+    const user = verifyToken(res.data.accessToken);
+    console.log(user);
+    dispatch(
+      setUser({
+        user: user,
+        token: res.data.accessToken,
+      })
+    );
+    console.log(res);
+  };
+  return (
+    <form onSubmit={handleSubmit(onsubmit)}>
+      <div>
+        <label htmlFor="id">ID:</label>
+        <input type="text" id="id" {...register("id")} />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input type="text" id="password" {...register("password")} />
+      </div>
+      <Button htmlType="submit">Submit</Button>
+    </form>
+  );
 };
 
 export default Login;

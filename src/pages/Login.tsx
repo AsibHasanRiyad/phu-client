92df34b4747f29a2de1c6aafa6d14234c30821ba
Asib Hasan Riyad <asibhasanriyad@gmail.com>
2024-07-12 11:56:52 +0600

    module 29 done

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,5 @@
-import { Button } from "antd";
-import { FieldValues, useForm, useFormContext } from "react-hook-form";
+import { Button, Row } from "antd";
+import { FieldValues } from "react-hook-form";
 import { useLoginMutation } from "../redux/features/auth/authApi";
 import { useAppDispatch } from "../redux/hooks";
 import { TUser, setUser } from "../redux/features/auth/authSlice";
@@ -18,44 +18,49 @@ const Login = () => {
   //     password: "12345",
   //   },
   // });
+
+  const defaultValues = {
+    id: "A-0001",
+    password: "12345",
+  };
   const [login] = useLoginMutation();
 
   const onsubmit = async (data: FieldValues) => {
     console.log(data);
-    // const toastId = toast.loading("User logging in..");
+    const toastId = toast.loading("User logging in..");
 
-    // try {
-    //   const userInfo = {
-    //     id: data.id,
-    //     password: data.password,
-    //   };
-    //   const res = await login(userInfo).unwrap();
-    //   const user = verifyToken(res.data.accessToken) as TUser;
-    //   dispatch(
-    //     setUser({
-    //       user: user,
-    //       token: res.data.accessToken,
-    //     })
-    //   );
-    //   toast.success("User logged in successfully", {
-    //     id: toastId,
-    //     duration: 2000,
-    //   });
-    //   navigate(`/${user.role}/dashboard`);
-    // } catch (error) {
-    //   toast.error("Something went wrong", { id: toastId, duration: 2000 });
-    // }
+    try {
+      const userInfo = {
+        id: data.id,
+        password: data.password,
+      };
+      const res = await login(userInfo).unwrap();
+      const user = verifyToken(res.data.accessToken) as TUser;
+      dispatch(
+        setUser({
+          user: user,
+          token: res.data.accessToken,
+        })
+      );
+      toast.success("User logged in successfully", {
+        id: toastId,
+        duration: 2000,
+      });
+      navigate(`/${user.role}/dashboard`);
+    } catch (error) {
+      toast.error("Something went wrong", { id: toastId, duration: 2000 });
+    }
   };
   return (
-    <PHForm onSubmit={onsubmit}>
-      <div>
+    <Row justify={"center"} align={"middle"} style={{ minHeight: "100vh" }}>
+      <PHForm onSubmit={onsubmit} defaultValues={defaultValues}>
         <PHInput type={"text"} name={"id"} label={"ID: "} />
-      </div>
-      <div>
+
         <PHInput type={"password"} name={"password"} label={"Password: "} />
-      </div>
-      <Button htmlType="submit">Submit</Button>
-    </PHForm>
+
+        <Button htmlType="submit">Submit</Button>
+      </PHForm>
+    </Row>
   );
 };
 

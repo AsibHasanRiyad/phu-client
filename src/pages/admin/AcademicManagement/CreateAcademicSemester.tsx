a4c798c1a7c247e3f6b3bd3ff471b6aa917f298e
Asib Hasan Riyad <asibhasanriyad@gmail.com>
2024-07-16 11:31:13 +0600

    ph select added dynimacylly

--- src/pages/admin/AcademicManagement/CreateAcademicSemester.tsx
+++ src/pages/admin/AcademicManagement/CreateAcademicSemester.tsx
@@ -1,17 +1,27 @@
 import { FieldValues, SubmitHandler } from "react-hook-form";
 import PHForm from "../../../components/form/PHForm";
-import PHInput from "../../../components/form/PHInput";
-import { Button } from "antd";
+import { Button, Col, Flex } from "antd";
+import PHSelect from "../../../components/form/PHSelect";
 
 const CreateAcademicSemester = () => {
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
     console.log(data);
   };
+  const nameOptions = [
+    { value: "Autumn", label: "Autumn" },
+    { value: "Summer", label: "Summer" },
+    { value: "Fall", label: "Fall" },
+  ];
   return (
-    <PHForm onSubmit={onSubmit}>
-      <PHInput type="text" name="test" label="Test" />
-      <Button htmlType="submit">Submit</Button>
-    </PHForm>
+    <Flex justify="center">
+      <Col span={6}>
+        <PHForm onSubmit={onSubmit}>
+          <PHSelect options={nameOptions} name={"name"} label={"Name"} />
+          {/* <PHSelect name={"year"} label={"Year"} /> */}
+          <Button htmlType="submit">Submit</Button>
+        </PHForm>
+      </Col>
+    </Flex>
   );
 };
 
